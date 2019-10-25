cd6054507777e0b6caa943f9fa3af183b6c8edc7
tintinjokes <43967697+tintinjokes@users.noreply.github.com>
2019-10-24 23:05:25 -0700

    Beta HUD. Chad HUD coming soon
    Its a simple addition that tells the player if they are walking or standing. It can be modified later for some sort of sound indication

---
+++ Stealth Game Collab/Assets/Tintin(The Next Hideo Kojima)/Scripts/HUD.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public string walking = "walking";
+    public string standing = "standing";
+    public Text stance;
+    bool isMoving = false;
+
+    public KeyCode moveLeft = KeyCode.A;
+    public KeyCode moveRight = KeyCode.D;
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public KeyCode interact = KeyCode.E;
+    public KeyCode dash = KeyCode.Space;
+    public KeyCode cover = KeyCode.LeftControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(moveLeft) || Input.GetKey(moveRight) || Input.GetKey(moveUp) || Input.GetKey(moveDown))
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        if (isMoving)
+        {
+            stance.text = walking;
+        }
+        else
+        {
+            stance.text = standing;
+        }
+    }
+}
