ab81679f160e1f323bf9c99b80159ba79538baf1
Mario <mariovictorino31@gmail.com>
2019-11-16 16:54:12 -0800

    Speaker Tests

---
+++ Stealth Game Collab/Assets/Mario/Scripts/SpeakerBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakerBehavior : MonoBehaviour
+{
+    public EnemyScript enemyScript;
+
+    void OnTriggerEnter2D()
+    {
+        if (gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown("E"))
+            {
+                enemyScript.heardSpeaker = true;
+                Debug.Log("What's That?");
+            }
+        }
+    }
+}
