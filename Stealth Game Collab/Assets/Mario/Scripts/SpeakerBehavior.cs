67381c91884203765b597c0203eda7a322f8abf5
Mario <mariovictorino31@gmail.com>
2019-11-19 12:15:13 -0800

    Updated Speaker

--- Stealth Game Collab/Assets/Mario/Scripts/SpeakerBehavior.cs
+++ Stealth Game Collab/Assets/Mario/Scripts/SpeakerBehavior.cs
@@ -6,15 +6,19 @@ public class SpeakerBehavior : MonoBehaviour
 {
     public EnemyScript enemyScript;
 
-    void OnTriggerEnter2D()
+    void OnTriggerEnter2D(Collider2D sp)
     {
-        if (gameObject.tag == "Player")
+        if (sp.gameObject.tag == "Player")
         {
-            if (Input.GetKeyDown("E"))
+            if (Input.GetKeyDown("e"))
             {
                 enemyScript.heardSpeaker = true;
                 Debug.Log("What's That?");
             }
+        }
+        if (sp.gameObject.tag == "Guard")
+        {
+            enemyScript.heardSpeaker = false;
         }
     }
 }
