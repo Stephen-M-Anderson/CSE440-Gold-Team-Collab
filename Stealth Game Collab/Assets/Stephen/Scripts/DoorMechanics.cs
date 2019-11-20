d4154daea0f714da7834edc7b377b13570cec78a
Stephen-M-Anderson <brokensteelstring@gmail.com>
2019-11-20 02:17:55 -0800

    Tinkered with doors, prototype finished

---
+++ Stealth Game Collab/Assets/Stephen/Scripts/DoorMechanics.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMechanics : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private Vector2 origPos;
+    private Quaternion origRot;
+    public bool isOpen;
+    public PlayerWalking player;
+    public HingeJoint2D hinge;
+    public float openingForce;
+    public float doorOpenRange;
+    public bool closing;
+    private JointMotor2D mt;
+    public float doorAngleDifference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = false;
+        origPos = transform.position;
+        origRot = transform.rotation;
+        mt.maxMotorTorque = 1000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        doorAngleDifference = Quaternion.Angle(transform.rotation, origRot); // lets you see how open the door is in the inspector
+        if (Input.GetKeyDown(player.interact)) // shows how far the player is from the object when the player hits e
+        {
+            Debug.Log("Distance: " + Vector2.Distance(transform.position, player.transform.position));
+        }
+        if (Input.GetKeyDown(player.interact) && isOpen == false && Vector2.Distance(transform.position, player.transform.position) < doorOpenRange) // if the door is closed and the player presses interact while in range, open the door
+        { 
+            isOpen = true;
+            rb.constraints = RigidbodyConstraints2D.None; // allows the door to move
+            mt.motorSpeed = openingForce; // turns on the doors motor and gives it a force
+            hinge.motor = mt;
+            hinge.useMotor = true;
+        }
+        else if (isOpen == true)
+        {
+            if (Quaternion.Angle(transform.rotation, origRot) < 1) // if the door is open but very close to being shut, shut the door fully
+            {
+                closing = false; 
+                isOpen = false;
+                hinge.useMotor = false;
+                transform.rotation = origRot; // reset the door to its shut position, aka it's original position
+                transform.position = origPos;
+                rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+            else if (Input.GetKeyDown(player.interact) && Vector2.Distance(transform.position, player.transform.position) < doorOpenRange) // if the door is open and the player presses interact while in range, close the door
+            {
+                closing = true;
+                hinge.useMotor = true;
+                mt.motorSpeed = 0 - (openingForce / 2); // play around with this number to make it close faster or slower. 
+                hinge.motor = mt;
+                hinge.useMotor = true;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isOpen)
+        {
+            if (hinge.motor.motorSpeed < 1 && !closing) // if the door is opening but very slowly, turn the motor off. She's done boys.
+            {
+                hinge.useMotor = false;
+            }
+            else if (!closing) // if the door is opening after the player has pressed interact
+            {
+                mt.motorSpeed = mt.motorSpeed - (mt.motorSpeed / 3); // rapidly slows the doors opening speed, gives it a "popped open" feel
+                hinge.motor = mt;
+            }
+        }
+    }
+}
