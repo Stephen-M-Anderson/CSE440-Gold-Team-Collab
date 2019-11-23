6ce2b6ceba60c2b5192fa6e09304e3fe3d1bb071
tintinjokes <43967697+tintinjokes@users.noreply.github.com>
2019-11-23 15:24:51 -0800

    Sort of an ALERT Hud
    asdasda

--- Stealth Game Collab/Assets/Tintin(The Next Hideo Kojima)/Scripts/AlertHUD.cs
+++ Stealth Game Collab/Assets/Tintin(The Next Hideo Kojima)/Scripts/AlertHUD.cs
@@ -8,13 +8,48 @@ public class AlertHUD : MonoBehaviour
     // Start is called before the first frame update
     public FlashlightDetection flashlightAlert;
     public Image alert;
+    int hasAlerted = 0;
+    float waitTimeNumber = 5;
+
+    private void Start()
+    {
+        StartCoroutine(waitandStuff());
+    }
     // Update is called once per frame
     void Update()
     {
-        if(flashlightAlert.enemyScript.inView)
+        if (flashlightAlert.enemyScript.inView)
         {
             alert.color = UnityEngine.Color.red;
             Debug.Log("IN VIEW IN VIEW");
+            hasAlerted++;
         }
+
+        else if (hasAlerted == 1 && !flashlightAlert.enemyScript.inView)
+        {
+            waitandStuff();
+        }
+        else if (hasAlerted > 1)
+        {
+
+        }
+
+
+
+    }
+
+    void calmDownPeriod()
+    {
+        alert.color = UnityEngine.Color.yellow;
+    }
+
+    private IEnumerator waitandStuff()
+    {
+        Debug.Log("BEGINNING THE WAIT");
+        yield return new WaitForSeconds(waitTimeNumber);
+
+        Debug.Log("END OF WAIT");
+        calmDownPeriod();
+        hasAlerted = 0;
     }
 }
