3985e4146990ae400ded8289a8a0b51fad5afdb2
Chrismorales116 <50187552+Chrismorales116@users.noreply.github.com>
2019-11-23 00:10:07 -0800

    Working on gettin that soda can working
    aaaaaaaaaaaaaaaaaa

--- Stealth Game Collab/Assets/Christian/Scripts/CanThrowingScript.cs
+++ Stealth Game Collab/Assets/Christian/Scripts/CanThrowingScript.cs
@@ -10,8 +10,9 @@ public class CanThrowingScript : MonoBehaviour
     public GameObject SodaCan;
     private float canDistanceOriginal;
     private bool isThrowing;
+    public bool hasCan;
     private Vector2 canStart;
-    public Rigidbody2D rb;
+    public Rigidbody2D rbSodaCan;
     public Rigidbody2D rbPlayer;
     public Transform spawnPoint;
 
@@ -28,14 +29,17 @@ public class CanThrowingScript : MonoBehaviour
         mousePosition.z = Mathf.Abs(0.0f - Camera.main.transform.position.z);
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         Vector2 direction = new Vector2(mousePosition.x - rbPlayer.position.x, mousePosition.y - rbPlayer.position.y);
+        //Vector2 direction = new Vector2(mousePosition.x, mousePosition.y);
         direction = direction.normalized;
 
-        if (Input.GetKeyDown(item) && !isThrowing)     
+        if (Input.GetKeyDown(item) && !isThrowing && hasCan)     
         {
             Instantiate(SodaCan, spawnPoint.position, spawnPoint.rotation);
-            rb.AddForce(direction * canSpeed, ForceMode2D.Impulse); //this is the rb of the can
+            //rbSodaCan = GetComponent<Rigidbody2D>(); //Pretty sure these two lines aren't needed I just added them to see if it solved my problem and it didn't
+            //rbPlayer = GetComponent<Rigidbody2D>();
+            rbSodaCan.AddForce(direction * canSpeed, ForceMode2D.Impulse); //this is the rb of the can
             isThrowing = true;
-            canStart = rbPlayer.position;
+            canStart = spawnPoint.position;
             canDistanceOriginal = canDistance;
             if (Vector2.Distance(mousePosition, canStart) < canDistance)
             {
@@ -44,7 +48,7 @@ public class CanThrowingScript : MonoBehaviour
         }
         if (isThrowing) 
         {
-            if (Vector2.Distance(rbPlayer.position, canStart) >= canDistance) 
+            if (Vector2.Distance(mousePosition, canStart) >= canDistance) 
             {
                 isThrowing = false;
                 rbPlayer.velocity = Vector2.zero;
