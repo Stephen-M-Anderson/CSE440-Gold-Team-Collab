af4cbfed25ba9e89599265ac5a22315d5a8a8ca9
Stephen-M-Anderson <49597928+Stephen-M-Anderson@users.noreply.github.com>
2019-11-24 16:39:24 -0800

    WaypointScript is now working
    Waypoints scan all other waypoints in the scene and link up to the closest ones automatically!

--- Stealth Game Collab/Assets/Stephen/Scripts/WaypointScript.cs
+++ Stealth Game Collab/Assets/Stephen/Scripts/WaypointScript.cs
@@ -4,42 +4,59 @@ using UnityEngine;
 
 public class WaypointScript : MonoBehaviour
 {
-    public int ArraySize;
-    private GameObject[] waypoints;
+    public int numberOfMapWaypoints;
+    public GameObject[] waypoints;
     public GameObject[] adjacentWaypoints;
     private GameObject[] tempNodeArray;
     private float currentNodeDistance;
     private float closestNodeDistance;
 
+    private int i, j;
+
     // Start is called before the first frame update
     void Start()
     {
+        waypoints = new GameObject[numberOfMapWaypoints];
+        tempNodeArray = new GameObject[numberOfMapWaypoints];
+        adjacentWaypoints = new GameObject[8];
         waypoints = GameObject.FindGameObjectsWithTag("Waypoint"); // grabs all the waypoints on the map and sticks them in the array
-        int i = 0;
         closestNodeDistance = 0;
+        i = 0;
+        
         foreach (GameObject node in waypoints) // iterates through the waypoints, each individual waypoint that it checks is called node
         {
             currentNodeDistance = Vector2.Distance(transform.position, node.transform.position);    // measures the distance between our main node (the one this script is attatched to)
                                                                                                     // and the node currently being iterated through
+            
+            if (node == this.gameObject)
+                continue;
             if (closestNodeDistance == 0) // if this is the first node we've checked...
             {
                 closestNodeDistance = currentNodeDistance;  // well then this node is technically the closest one so lets just put it in that temp array yea?
-                tempNodeArray[i] = node;                    // bam it's in there
+                tempNodeArray[i] = node;         // bam it's in there
                 i++; // Do it yourself for-loop, baby!
             }
-            else if (currentNodeDistance <= closestNodeDistance) // if this node is the closest node that we've checked, or as far as the closest node...
+
+            else if (currentNodeDistance < 3f)
             {
-                closestNodeDistance = currentNodeDistance; // make it official, this one is the closest node
-                tempNodeArray[i] = node; // pop that close ass node in that temp array
-                i++; // Do it yourself for-loop, baby!
+                tempNodeArray[i] = node;
+                i++;
+                if (currentNodeDistance < closestNodeDistance) 
+                    closestNodeDistance = currentNodeDistance;
             }
         }
-        float diagonalDistance = Mathf.Sqrt(closestNodeDistance * closestNodeDistance * 2); // some trig so we can connect nodes diagonally, uses rules for a 45-45-90 triangle
-                                                                                            // A^2 + B^2 = C^2, A = B -> C = sqrt ( A * A * 2)
-
-        foreach (GameObject node in tempNodeArray) // now we go through the temp array and find which nodes are closest
+                                                                                                    
+        j = 0;  // index for the adjacentWaypoints array
+        foreach (GameObject node in tempNodeArray) // now we go through the temp array and find which nodes are actually the closest
         {
-
+            if (node == null)
+                continue;
+            currentNodeDistance = Vector2.Distance(transform.position, node.transform.position); // get distance from this node to the node in the array
+            if (currentNodeDistance < 3f) // if that node is either closest or diagnoal, pop it in the array!
+            {
+                adjacentWaypoints[j] = node;
+                j++;
+            }
         }
     }
 
