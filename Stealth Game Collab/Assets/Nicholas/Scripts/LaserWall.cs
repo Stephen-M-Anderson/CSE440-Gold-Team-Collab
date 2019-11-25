fa102a7bf42133192534a1e102b251aaa2b9518e
eldt <48176015+eldt@users.noreply.github.com>
2019-11-25 01:49:42 -0800

    Guard Investigation

--- Stealth Game Collab/Assets/Nicholas/Scripts/LaserWall.cs
+++ Stealth Game Collab/Assets/Nicholas/Scripts/LaserWall.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class LaserWall : MonoBehaviour
 {
-    private bool alarmSet;
+    public bool guardIsWaiting;
+    public NicEnemyScript enemyScript;
+    public GameObject patrollingGuard;
+    public GameObject laserWall;
+    public float guardTimer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +19,30 @@ public class LaserWall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (guardIsWaiting)
+        {
+            guardTimer += Time.deltaTime;
+        }
+
+        if (guardTimer >= 3)
+        {
+            guardTimer = 0;
+            guardIsWaiting = false;
+            enemyScript.laserTriggered = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            alarmSet = true;
             Debug.Log("Alarm Sent");
+            enemyScript.laserTriggered = true;
+        }
+
+        if (collision.gameObject.tag == "Guard")
+        {
+            guardIsWaiting = true;
         }
     }
 }
--- Stealth Game Collab/Assets/Nicholas/Scripts/SecCam.cs
+++ Stealth Game Collab/Assets/Nicholas/Scripts/SecCam.cs
@@ -6,12 +6,18 @@ public class SecCam : MonoBehaviour
 {
     public Transform camCenter;
     public LayerMask PlayerDetection;
+    public LayerMask GuardDetection;
     public float trackingSpeed = 40.0f; //Speed of the Camera
+    public GameObject patrolGuard; //Guard that will respond to Camera.
+    public GameObject secCamera;
 
+    public NicEnemyScript enemyScript;
 
     private Transform playerPosition;
     private bool isTrackingPlayer = false;
     private bool isOverlapped = false;
+    private bool guardIsWaiting = false;
+    public float guardTimer = 0.0f;
     void Start()
     {
         playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
@@ -20,8 +26,8 @@ public class SecCam : MonoBehaviour
     void Update()
     {
         isOverlapped = Physics2D.OverlapCircle(camCenter.position, 0.5f, PlayerDetection); //Player detection
+        guardIsWaiting = Physics2D.OverlapCircle(camCenter.position, 0.5f, GuardDetection);
 
-        Debug.Log("Overlap is " + isOverlapped);
         if (isTrackingPlayer == false)
         {
             transform.Rotate((Vector3.forward * trackingSpeed * Time.deltaTime)); //rotates camera continuously
@@ -34,6 +40,19 @@ public class SecCam : MonoBehaviour
 
         }
 
+        if (guardIsWaiting)
+        {
+            guardTimer += Time.deltaTime;
+        }
+        else
+        {
+            guardTimer = 0;
+        }
+        if (guardTimer >= 6)
+        {
+            enemyScript.cameraSpotted = false;
+            guardTimer = 0;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -51,6 +70,7 @@ public class SecCam : MonoBehaviour
         {
             isTrackingPlayer = true;
             Debug.Log("Player spotted by camera");
+            enemyScript.cameraSpotted = true;
         }
     }
     private void OnTriggerStay2D(Collider2D collision)
@@ -59,6 +79,7 @@ public class SecCam : MonoBehaviour
         {
             isTrackingPlayer = true;
             Debug.Log("Player spotted by camera");
+            enemyScript.cameraSpotted = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
