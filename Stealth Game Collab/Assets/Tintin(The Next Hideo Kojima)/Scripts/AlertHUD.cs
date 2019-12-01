14f1fbf4b236a0fa91961c102cdc5c8db916ed2c
tintinjokes <43967697+tintinjokes@users.noreply.github.com>
2019-11-30 19:04:34 -0800

    pushy

--- Stealth Game Collab/Assets/Tintin(The Next Hideo Kojima)/Scripts/AlertHUD.cs
+++ Stealth Game Collab/Assets/Tintin(The Next Hideo Kojima)/Scripts/AlertHUD.cs
@@ -5,51 +5,55 @@ using UnityEngine.UI;
 
 public class AlertHUD : MonoBehaviour
 {
+    public Color redColor, yellowColor;
+    Color currentColor;
+    MeshRenderer coloerer;
     // Start is called before the first frame update
-    public FlashlightDetection flashlightAlert;
+    public GetCaughtDetection getcaughtbool;
     public Image alert;
     int hasAlerted = 0;
-    float waitTimeNumber = 5;
+    float waitTimeNumber = 5.0f;
+    bool doingthings = false; // This bool will STOP the fucking courtine function from doings things
 
     private void Start()
     {
-        StartCoroutine(waitandStuff());
+        
+        //wStartCoroutine(waitandStuff(doingthings));
     }
     // Update is called once per frame
     void Update()
     {
-        if (flashlightAlert.enemyScript.inView)
+        if (getcaughtbool.Sighted)  //IF SIGHTED by enemy
         {
-            alert.color = UnityEngine.Color.red;
+            alert.color = UnityEngine.Color.red;   //MAKE THE HUD SCARY RED
             Debug.Log("IN VIEW IN VIEW");
-            hasAlerted++;
+            hasAlerted = 1;
+            //hasAlerted++;
+            //StartCoroutine(waitandStuff());
         }
-
-        else if (hasAlerted == 1 && !flashlightAlert.enemyScript.inView)
+        
+        else if (!getcaughtbool.Sighted && hasAlerted == 1)
         {
-            waitandStuff();
+            //StartCoroutine(waitandStuff(doingthings));
+            //doingthings = true;
+            //Debug.Log("ITS TIME TO CHANGE COLOR");
+            
+            alert.color = Color.Lerp(alert.color, Color.yellow, 0.01f);
+            
         }
-        else if (hasAlerted > 1)
-        {
-
-        }
-
+        
 
 
     }
-
-    void calmDownPeriod()
-    {
-        alert.color = UnityEngine.Color.yellow;
-    }
-
+    /*
     private IEnumerator waitandStuff()
-    {
-        Debug.Log("BEGINNING THE WAIT");
-        yield return new WaitForSeconds(waitTimeNumber);
-
-        Debug.Log("END OF WAIT");
-        calmDownPeriod();
-        hasAlerted = 0;
+    { 
+            Debug.Log("BEGINNING THE WAIT which equals" + waitTimeNumber);
+            yield return new WaitForSeconds(waitTimeNumber);
+            Debug.Log("Done waiting");
+            doingthings = false;
+            //calmDownPeriod()
     }
+    
+    */
 }
