fb56a936c2018e6333c2bf81ed41d26df817826b
Mario Victorino <mariovictorino31@gmail.com>
2019-11-30 19:41:38 -0800

    Guard Chase Tests

--- Stealth Game Collab/Assets/Mario/Scripts/FlashlightDetection.cs
+++ Stealth Game Collab/Assets/Mario/Scripts/FlashlightDetection.cs
@@ -5,22 +5,38 @@ using UnityEngine;
 public class FlashlightDetection : MonoBehaviour
 {
     public EnemyScript enemyScript;
+    public float chaseTimer;
 
+    private void Start()
+    {
+        chaseTimer = 30;
+    }
+
+    private void Update()
+    {
+        if (enemyScript.inView == true)
+        {
+            chaseTimer--;
+        }
+    }
     void OnTriggerEnter2D(Collider2D o)
     {
         if (o.gameObject.tag == "Player")
         {
             enemyScript.inView = true;
             Debug.Log("HEY");
+            enemyScript.speed = 3;
+            chaseTimer = 30;
         }
     }
 
     void OnTriggerExit2D(Collider2D o)
     {
-        if (o.gameObject.tag == "Player")
+        if (o.gameObject.tag == "Player" && chaseTimer > 0)
         {
             enemyScript.inView = false;
             Debug.Log("Must Have Been the Wind");
+            enemyScript.speed = 2;
         }
     }
 }
